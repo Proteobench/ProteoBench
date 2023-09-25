92214d86cc356fcfb638d4452799361e46249235
RobbinBouwmeester <Robbin.bouwmeester@ugent.be>
2023-09-25 13:28:17 +0200

    Update test_module_dda_quant.py

--- test/test_module_dda_quant.py
+++ test/test_module_dda_quant.py
@@ -14,7 +14,7 @@ from proteobench.modules.dda_quant.plot import PlotDataPoint
 
 TESTDATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 TESTDATA_FILES = {
-    "WOMBAT": os.path.join(TESTDATA_DIR, "WOMBAT_stand_pep_quant_mergedproline.csv"),
+    #"WOMBAT": os.path.join(TESTDATA_DIR, "WOMBAT_stand_pep_quant_mergedproline.csv"),
     "MaxQuant": os.path.join(TESTDATA_DIR, "MaxQuant_evidence_sample.txt"),
     "MSFragger": os.path.join(TESTDATA_DIR, "MSFragger_combined_ion.tsv"),
     "AlphaPept": os.path.join(TESTDATA_DIR, "AlphaPept_subset.csv"),

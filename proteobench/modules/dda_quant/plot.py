14eab946adc167a5ccdfbf999eabcb6f9cb6c853
Witold Wolski <wewolski@gmail.com>
2023-09-25 16:45:27 +0200

    adding gridlines to the figures #45

--- proteobench/modules/dda_quant/plot.py
+++ proteobench/modules/dda_quant/plot.py
@@ -27,7 +27,7 @@ class PlotDataPoint:
         fig.update_layout(
             width=700,
             height=700,
-            title="Distplot",
+            # title="Distplot",
             xaxis=dict(
                 title="1|2_ratio",
                 color="white",
@@ -40,6 +40,8 @@ class PlotDataPoint:
             ),
         )
         fig.update_xaxes(range=[0, 4])
+        fig.update_xaxes(showgrid=True, gridcolor="lightgray", gridwidth=1)
+        # fig.update_yaxes(showgrid=True, gridcolor="lightgray", gridwidth=1)
 
         return fig
 
@@ -75,18 +77,17 @@ class PlotDataPoint:
 
         # Add hover text
         hover_texts = [
-            f"Search Engine: {benchmark_metrics_df.search_engine[idx]} {benchmark_metrics_df.software_version[idx]}<br>" + 
-            f"FDR psm: {benchmark_metrics_df.fdr_psm[idx]}<br>" +
-            f"FDR Peptide: {benchmark_metrics_df.fdr_peptide[idx]}<br>" + 
-            f"FRD Protein: {benchmark_metrics_df.fdr_protein[idx]}<br>" +
-            f"MBR: {benchmark_metrics_df.MBR[idx]}<br>" + 
-            f"Precursor Tolerance: {benchmark_metrics_df.precursor_tol[idx]} {benchmark_metrics_df.precursor_tol_unit[idx]}<br>" + 
-            f"Fragment Tolerance: {benchmark_metrics_df.fragment_tol_unit[idx]}<br>" + 
-            f"Enzyme: {benchmark_metrics_df.enzyme_name[idx]} <br>" + 
-            f"Missed Cleavages: {benchmark_metrics_df.missed_cleavages[idx]}<br>" + 
-            f"Min peptide length: {benchmark_metrics_df.min_pep_length[idx]}<br>" + 
-            f"Max peptide length: {benchmark_metrics_df.max_pep_length[idx]}"
-                    
+            f"Search Engine: {benchmark_metrics_df.search_engine[idx]} {benchmark_metrics_df.software_version[idx]}<br>"
+            + f"FDR psm: {benchmark_metrics_df.fdr_psm[idx]}<br>"
+            + f"FDR Peptide: {benchmark_metrics_df.fdr_peptide[idx]}<br>"
+            + f"FRD Protein: {benchmark_metrics_df.fdr_protein[idx]}<br>"
+            + f"MBR: {benchmark_metrics_df.MBR[idx]}<br>"
+            + f"Precursor Tolerance: {benchmark_metrics_df.precursor_tol[idx]} {benchmark_metrics_df.precursor_tol_unit[idx]}<br>"
+            + f"Fragment Tolerance: {benchmark_metrics_df.fragment_tol_unit[idx]}<br>"
+            + f"Enzyme: {benchmark_metrics_df.enzyme_name[idx]} <br>"
+            + f"Missed Cleavages: {benchmark_metrics_df.missed_cleavages[idx]}<br>"
+            + f"Min peptide length: {benchmark_metrics_df.min_pep_length[idx]}<br>"
+            + f"Max peptide length: {benchmark_metrics_df.max_pep_length[idx]}"
             for idx, row in benchmark_metrics_df.iterrows()
         ]
 
@@ -101,14 +102,16 @@ class PlotDataPoint:
                     y=benchmark_metrics_df["nr_prec"],
                     mode="markers",
                     text=hover_texts,
-                    marker=dict(color=colors, showscale=True, size=20),
-                    marker_size=[mapping[item] for item in benchmark_metrics_df["old_new"]]
+                    marker=dict(color=colors, showscale=False, size=20),
+                    marker_size=[
+                        mapping[item] for item in benchmark_metrics_df["old_new"]
+                    ],
                 )
             ]
         )
 
         fig.update_layout(
-            title="Metric",
+            # title="Metric",
             width=700,
             height=700,
             xaxis=dict(
@@ -121,10 +124,11 @@ class PlotDataPoint:
                 gridcolor="white",
                 gridwidth=2,
             ),
-            paper_bgcolor='rgb(243, 243, 243)',
-            plot_bgcolor='rgb(243, 243, 243)'
+            # paper_bgcolor='rgb(243, 243, 243)',
+            # plot_bgcolor="rgb(243, 243, 243)",
         )
-
+        fig.update_xaxes(showgrid=True, gridcolor="lightgray", gridwidth=1)
+        fig.update_yaxes(showgrid=True, gridcolor="lightgray", gridwidth=1)
         # selected_points = plotly_events(
         #    fig,
         #    select_event=True,

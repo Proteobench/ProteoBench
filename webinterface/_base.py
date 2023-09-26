ab32547f53e45fd8e77aa367a8f02939ec48faa6
Marie Locard-Paulet <8386272+mlocardpaulet@users.noreply.github.com>
2023-09-26 12:31:30 +0200

    add main logos in base.py

--- webinterface/_base.py
+++ webinterface/_base.py
@@ -42,25 +42,15 @@ class StreamlitPage(ABC):
 
     def _sidebar(self):
         """Format sidebar."""
-        st.sidebar.markdown(
-            """
-            # Proteobench
-            """
-        )
-        logo_list = [
-            "https://github.com/Proteobench/ProteoBench/blob/Add-logos/webinterface/logos/logo_participants/ADlab.png?raw=true",
-            "https://github.com/Proteobench/ProteoBench/blob/Add-logos/webinterface/logos/logo_participants/CBMR.png?raw=true",
-            "https://github.com/Proteobench/ProteoBench/blob/Add-logos/webinterface/logos/logo_participants/MetaproteomicsInitiative.png?raw=true",
-            "https://github.com/Proteobench/ProteoBench/blob/Add-logos/webinterface/logos/logo_participants/ULaval.png?raw=true",
-            "https://github.com/Proteobench/ProteoBench/blob/Add-logos/webinterface/logos/logo_participants/Viki_FH%20Logo%20international%20mit%20University_jpg.jpg?raw=true",
-            "https://github.com/Proteobench/ProteoBench/blob/Add-logos/webinterface/logos/logo_participants/logo_UGent_EN_RGB_2400_color-on-white.png?raw=true",
-            "https://github.com/Proteobench/ProteoBench/blob/Add-logos/webinterface/logos/logo_participants/netherlands-escience-center-logo-RGB.png?raw=true",
-            "https://github.com/Proteobench/ProteoBench/blob/Add-logos/webinterface/logos/logo_participants/sdulogo_da.png?raw=true",
-            "https://github.com/Proteobench/ProteoBench/blob/Add-logos/webinterface/logos/logo_participants/vib.png?raw=true"
+        st.sidebar.image("https://github.com/Proteobench/ProteoBench/raw/Add-logos/webinterface/logos/logo_proteobench/proteobench-logo-horizontal.svg",
+                width=300)
+        
+        mainlogo_list = [
+            "https://github.com/Proteobench/ProteoBench/blob/Add-logos/webinterface/logos/logo_funding/eubic_logo_transparent.png?raw=true",
+            "https://github.com/Proteobench/ProteoBench/blob/Add-logos/webinterface/logos/logo_funding/eupa-logo-transparent.png?raw=true"
         ]
-
-        for i in range(0,len(logo_list),3):
+        for i in range(0,len(mainlogo_list),3):
             st.sidebar.image(
-                logo_list[i:i+3],
-                width=75,
+                mainlogo_list[i:i+3],
+                width=300,
             )

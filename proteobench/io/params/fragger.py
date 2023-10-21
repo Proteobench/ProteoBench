fcce49ff61eda45bda0c1655a3635c192af359a5
Henry <henry.webel@cpr.ku.dk>
2023-10-21 15:16:49 +0200

    :sparkles: Parse MSFragger parameter files

---
+++ proteobench/io/params/fragger.py
@@ -0,0 +1,60 @@
+"""Functionality to parse MSFragger fragger.params parameter files.
+
+MSFragger has a text based paramter file format which 
+separates paramters and their value using an equal sign. Optional comments are 
+expressed with a hash sign.
+"""
+from __future__ import annotations
+
+import logging
+from collections import namedtuple
+
+logger = logging.getLogger(__name__)
+
+Parameter = namedtuple("Parameter", ["name", "value", "comment"])
+
+
+def read_file(file: str) -> list[Parameter]:
+    """Read MSFragger parameter file as list of records."""
+    with open(file) as f:
+        data = []
+        for line in f:
+            line = line.strip()
+            logger.debug(line)
+            # ! logic below also allows to keep the comments as comments
+            if line.startswith("#"):
+                continue
+            if not line:
+                continue
+            if "#" in line:
+                res = line.split("#")
+                if len(res) == 1:
+                    comment = res[0]
+                    data.append(Parameter(None, None, comment.strip()))
+                    continue
+                param, comment = [x.strip() for x in res]
+            else:
+                param = line
+                comment = None
+            res = param.strip().split(" = ")
+            if len(res) == 1:
+                param = res[0].strip()
+                data.append(Parameter(param, None, comment))
+                continue
+            param, value = [x.strip() for x in res]
+            data.append(Parameter(param, value, comment))
+    return data
+
+
+if __name__ == "__main__":
+    import pathlib
+
+    import pandas as pd
+
+    file = pathlib.Path("../../../test/params/fragger.params")
+    data = read_file(file)
+    df = pd.DataFrame.from_records(data, columns=Parameter._fields).set_index(
+        Parameter._fields[0]
+    )
+    df
+    df.to_csv(file.with_suffix(".csv"))
---
+++ test/test_parse_params_fragger.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import pandas as pd
+
+import proteobench.io.params.fragger as fragger_params
+
+TESTDATA_DIR = Path(__file__).parent / "params"
+
+
+def test_read_file():
+    file = TESTDATA_DIR / "fragger.params"
+    csv_expected = TESTDATA_DIR / "fragger.csv"
+    expected = pd.read_csv(csv_expected)
+    data = fragger_params.read_file(file)
+    actual = pd.DataFrame.from_records(
+        data, columns=(fragger_params.Parameter._fields)
+    ).set_index(fragger_params.Parameter._fields[0])
+    actual.equals(expected)

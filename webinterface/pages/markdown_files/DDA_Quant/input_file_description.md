5290b34fdfaaa829f2958024975416a9a9f8509b
Alireza Nameni <alireza_nameni@yahoo.com>
2024-09-03 13:43:27 +0200

    DIA_ion beta page
    I made a beta version of DIA_ion page. I also made some changes to documentation of previous DDA beta pages.

--- webinterface/pages/markdown_files/DDA_Quant/input_file_description.md
+++ webinterface/pages/markdown_files/DDA_Quant/input_file_description.md
@@ -1,6 +1,6 @@
 Please upload the ouput of your analysis, and indicate what software 
 tool it comes from (this is necessary to correctly parse your table - find 
-more information in the "[How to use](https://proteobench.readthedocs.io/en/latest/modules/3-DDA-Quantification-ion-level/)" 
+more information in the "[How to use](https://proteobench.readthedocs.io/en/latest/modules/2-DDA-Quantification-ion-level/)"
 section of this module).
 
 Currently, we support output files from AlphaPept, MaxQuant, Proline and Sage. It is also possible to reformat your data in a tab-delimited file that we call "custom format" in the [documentation](https://proteobench.readthedocs.io/en/latest/modules/3-DDA-Quantification-ion-level/). We are still working on FragPipe and i2MassChroQ.
---
+++ webinterface/pages/pages_variables/dia_quant_variables.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass, field
+from typing import List, Type
+
+from pages.texts.generic_texts import WebpageTexts
+
+
+@dataclass
+class VariablesDIAQuant:
+    all_datapoints: str = "all_datapoints"
+    all_datapoints_submission: str = "all_datapoints_submission"
+    input_df_submission: str = "input_df_submission"
+    result_performance_submission: str = "result_performance_submission"
+    submit: str = "submit"
+    fig_logfc: str = "fig_logfc"
+    fig_metric: str = "fig_metric"
+    fig_cv: str = "fig_CV_violinplot"
+    result_perf: str = "result_perf"
+    meta_data: str = "meta_data"
+    input_df: str = "input_df"
+    meta_file_uploader_uuid: str = "meta_file_uploader_uuid"
+    comments_submission_uuid: str = "comments_submission_uuid"
+    check_submission_uuid: str = "check_submission_uuid"
+    meta_data_text: str = "comments_for_submission"
+    check_submission: str = "heck_submission"
+    button_submission_uuid: str = "button_submission_uuid"
+    df_head: str = "df_head"
+    placeholder_fig_compare: str = "placeholder_fig_compare"
+    placeholder_table: str = "placeholder_table"
+    placeholder_slider: str = "placeholder_slider"
+    highlight_list: List[str] = field(default_factory=list)
+    first_new_plot: bool = True
+    default_val_slider: int = 3
+    beta_warning: bool = True
+    github_link_pr: str = "github.com/Proteobot/Results_Module2_quant_DIA.git"
+
+    additional_params_json: str = "../webinterface/configuration/dia_quant.json"
+
+    description_module_md: str = "pages/markdown_files/DIA_Quant/introduction.md"
+    description_files_md: str = "pages/markdown_files/DIA_Quant/file_description.md"
+    description_input_file_md: str = "pages/markdown_files/DIA_Quant/input_file_description.md"
+    description_slider_md: str = "pages/markdown_files/DIA_Quant/slider_description.md"
+    description_table_md: str = "pages/markdown_files/DIA_Quant/table_description.md"
+    description_results_md: str = "pages/markdown_files/DIA_Quant/result_description.md"
+    description_submission_md: str = "pages/markdown_files/DIA_Quant/submit_description.md"
+
+    texts: Type[WebpageTexts] = WebpageTexts

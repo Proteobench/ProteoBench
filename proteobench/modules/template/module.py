de76f142d356cbc59b1838d94b2f50e659cc3d89
cajac102 <ca.jachmann@gmail.com>
2024-09-05 13:37:13 +0200

    remove abstract moduleInterface class, add documentation

--- proteobench/modules/template/module.py
+++ proteobench/modules/template/module.py
@@ -1,28 +1,40 @@
 from __future__ import annotations
 
 import datetime
-import itertools
-import re
 from dataclasses import asdict
 
 import pandas as pd
-import toml
 
-from proteobench.modules.dda_quant import ModuleInterface
+from typing import Any, Optional, Tuple
+
 from proteobench.modules.template.datapoint import Datapoint
 from proteobench.modules.template.parse import ParseInputs
 from proteobench.modules.template.parse_settings import TEMPLATE_RESULTS_PATH, ParseSettings
 
 
-class Module(ModuleInterface):
-    """Object is used as a main interface with the Proteobench library within the module."""
+class Module:
+    """Description of the Module."""
 
     def is_implemented() -> bool:
         """Returns whether the module is fully implemented."""
         return True
 
     def generate_intermediate(standard_format: dict, parse_settings: ParseSettings) -> pd.DataFrame:
-        """Calculate intermediate values from the uploaded file."""
+        """
+        Calculate intermediate values from the uploaded file.
+
+        Parameters
+        ----------
+        standard_format
+            The uploaded file in a standard format.
+        parse_settings
+            The settings used to parse the uploaded file.
+
+        Returns
+        -------
+        intermediate
+            The intermediate values calculated from the uploaded file.
+        """
 
         # TODO calculate intermediate values
         intermediate = pd.DataFrame()
@@ -30,7 +42,24 @@ class Module(ModuleInterface):
         return intermediate
 
     def generate_datapoint(intermediate: pd.DataFrame, input_format: str, user_input: dict) -> Datapoint:
-        """Method used to compute benchmarks for the provided intermediate structure."""
+        """
+        Method used to compute benchmarks for the provided intermediate structure.
+
+        Parameters
+        ----------
+        intermediate
+            The intermediate data structure.
+        input_format
+            The format of the input file.
+        user_input
+            The user input settings.
+
+        Returns
+        -------
+        df
+            The computed benchmark values.
+        """
+
         # Leave these lines as they are
         result_datapoint = Datapoint(
             id=input_format + "_" + user_input["version"] + "_" + str(datetime.datetime.now()),
@@ -55,7 +84,22 @@ class Module(ModuleInterface):
         return df
 
     def load_input_file(input_csv: str, input_format: str) -> pd.DataFrame:
-        """Method loads dataframe from a input file depending on its format."""
+        """
+        Method loads dataframe from a input file depending on its format.
+
+        Parameters
+        ----------
+        input_csv
+            The path to the input file.
+        input_format
+            The format of the input file.
+
+        Returns
+        -------
+        input_data_frame
+            The dataframe loaded from the input file.
+        """
+
         input_data_frame: pd.DataFrame
 
         # Format1 are the results from e.g. different search engines
@@ -67,8 +111,25 @@ class Module(ModuleInterface):
 
         return input_data_frame
 
-    def add_current_data_point(self, all_datapoints, current_datapoint):
-        """Add current data point to all data points and load them from file if empty"""
+    def add_current_data_point(
+        self, current_datapoint: pd.Series, all_datapoints: Optional[pd.DataFrame] = None
+    ) -> pd.DataFrame:
+        """
+        Add current data point to all data points and load them from file if empty.
+
+        Parameters
+        ----------
+        all_datapoints
+            The data points from previous runs.
+        current_datapoint
+            The current data point to be added.
+
+        Returns
+        -------
+        all_datapoints
+            The data points with the current data point added.
+        """
+
         if not isinstance(all_datapoints, pd.DataFrame):
             all_datapoints = pd.read_json(TEMPLATE_RESULTS_PATH)
         else:
@@ -77,8 +138,28 @@ class Module(ModuleInterface):
         all_datapoints = all_datapoints.T.reset_index(drop=True)
         return all_datapoints
 
-    def benchmarking(self, input_file: str, input_format: str, user_input: dict, all_datapoints) -> pd.DataFrame:
-        """Main workflow of the module. Used to benchmark workflow results."""
+    def benchmarking(self, input_file: str, input_format: str, user_input: dict, all_datapoints):
+        """
+        Main workflow of the module. Used to benchmark workflow results.
+
+        Parameters
+        ----------
+        input_file
+            Path to the workflow output file.
+        input_format
+            Format of the workflow output file.
+        user_input
+            User provided parameters for plotting.
+        all_datapoints
+            DataFrame containing all datapoints from the proteobench repo.
+        default_cutoff_min_prec
+            Minimum number of runs an ion has to be identified in.
+
+        Returns
+        -------
+        tuple[DataFrame, DataFrame]
+            Tuple containing the intermediate data structure, and all datapoints.
+        """
 
         # Read input file
         # call load_input_file() method

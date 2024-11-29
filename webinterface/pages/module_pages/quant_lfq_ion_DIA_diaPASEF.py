d242dfb5bd34bbbc73261248602c75bdc7942b89
rodvrees <robbe.devreese@hotmail.com>
2024-11-29 16:24:35 +0100

    Revert inheritance, rename final custom toml

--- webinterface/pages/module_pages/quant_lfq_ion_DIA_diaPASEF.py
+++ webinterface/pages/module_pages/quant_lfq_ion_DIA_diaPASEF.py
@@ -4,7 +4,6 @@ from typing import Any, Dict, Type
 import pages.texts.proteobench_builder as pbb
 import streamlit as st
 from pages.base_pages.quant import QuantUIObjects
-from pages.module_pages.quant_lfq_ion_DIA_AIF import StreamlitUI
 from pages.pages_variables.Quant.lfq.ion.DIA.ion_diaPASEF import (
     VariablesDIAQuantdiaPASEF,
 )
@@ -16,7 +15,7 @@ from proteobench.modules.quant.lfq.ion.DIA.quant_lfq_ion_DIA_diaPASEF import (
 )
 
 
-class StreamlitUIdiaPASEF(StreamlitUI):
+class StreamlitUI:
     def __init__(self):
         self.variables_dia_quant: VariablesDIAQuantdiaPASEF = VariablesDIAQuantdiaPASEF()
         self.texts: Type[WebpageTexts] = WebpageTexts
@@ -40,7 +39,88 @@ class StreamlitUIdiaPASEF(StreamlitUI):
 
         self._main_page()
 
+    def _main_page(self) -> None:
+        """
+        Sets up the main page layout for the Streamlit application.
+        """
+        # Create tabs
+        (
+            tab_results_all,
+            tab_submission_details,
+            tab_indepth_plots,
+            tab_results_new,
+            tab_public_submission,
+        ) = st.tabs(
+            [
+                "Public Benchmark Runs",
+                "Submit New Data",
+                "Results In-Depth",
+                "Results New Data",
+                "Public Submission",
+            ]
+        )
+
+        # Tab 1: Results (All Data)
+        with tab_results_all:
+            st.title(self.variables_dia_quant.texts.ShortMessages.title)
+            st.write(
+                f"The full description of the module is available [here]({self.variables_dia_quant.texts.ShortMessages.doc_url})"
+            )
+            if self.variables_dia_quant.beta_warning:
+                st.warning(
+                    "This module is in BETA phase. The figure presented below and the metrics calculation may change in the near future."
+                )
+            self.quant_uiobjects.display_all_data_results_main()
+
+        # Tab 2: Submission Details
+        with tab_submission_details:
+            st.title(self.variables_dia_quant.texts.ShortMessages.title)
+            st.write(
+                f"The full description of the module is available [here]({self.variables_dia_quant.texts.ShortMessages.doc_url})"
+            )
+            if self.variables_dia_quant.beta_warning:
+                st.warning(
+                    "This module is in BETA phase. The figure presented below and the metrics calculation may change in the near future."
+                )
+            self.quant_uiobjects.display_submission_form()
+
+        # Tab 2.5: in-depth plots current data
+        with tab_indepth_plots:
+            st.title(self.variables_dia_quant.texts.ShortMessages.title)
+            st.write(
+                f"The full description of the module is available [here]({self.variables_dia_quant.texts.ShortMessages.doc_url})"
+            )
+            if self.variables_dia_quant.beta_warning:
+                st.warning(
+                    "This module is in BETA phase. The figure presented below and the metrics calculation may change in the near future."
+                )
+            self.quant_uiobjects.generate_current_data_plots(True)
+
+        # Tab 3: Results (New Submissions)
+        with tab_results_new:
+            st.title(self.variables_dia_quant.texts.ShortMessages.title)
+            st.write(
+                f"The full description of the module is available [here]({self.variables_dia_quant.texts.ShortMessages.doc_url})"
+            )
+            if self.variables_dia_quant.beta_warning:
+                st.warning(
+                    "This module is in BETA phase. The figure presented below and the metrics calculation may change in the near future."
+                )
+            self.quant_uiobjects.display_all_data_results_submitted()
+
+        # Tab 4: Public Submission
+        with tab_public_submission:
+            st.title(self.variables_dia_quant.texts.ShortMessages.title)
+            st.write(
+                f"The full description of the module is available [here]({self.variables_dia_quant.texts.ShortMessages.doc_url})"
+            )
+            if self.variables_dia_quant.beta_warning:
+                st.warning(
+                    "This module is in BETA phase. The figure presented below and the metrics calculation may change in the near future."
+                )
+            self.quant_uiobjects.display_public_submission_ui()
+
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
-    StreamlitUIdiaPASEF()  # Instantiate and run the extended UI class
+    StreamlitUI()  # Instantiate and run the extended UI class

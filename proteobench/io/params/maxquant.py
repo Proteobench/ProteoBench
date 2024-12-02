d2efef5d448b0f5165fbc31d81b1277b46562f03
RobbinBouwmeester <robbin.bouwmeester@ugent.be>
2024-12-02 16:28:12 +0100

    Update maxquant.py

--- proteobench/io/params/maxquant.py
+++ proteobench/io/params/maxquant.py
@@ -1,4 +1,4 @@
-"""Functionality to parse Maxqunt mqpar.xml parameter files"""
+"""Functionality to parse Maxquant mqpar.xml parameter files"""
 
 from __future__ import annotations
 
@@ -6,6 +6,7 @@ import collections
 import logging
 import xml.etree.ElementTree as ET
 from pathlib import Path
+from typing import Any, Dict, List
 
 import numpy as np
 import pandas as pd
@@ -15,8 +16,21 @@ from proteobench.io.params import ProteoBenchParameters
 logger = logging.getLogger()
 
 
-def extend_tuple(t, target_length: int):
-    """Extend tuple with None values to match target length."""
+def extend_tuple(t: tuple, target_length: int) -> tuple:
+    """
+    Extend a tuple with `None` values to match the target length.
+
+    Args:
+        t (tuple): The original tuple.
+        target_length (int): The target length for the extended tuple.
+
+    Returns:
+        tuple: The extended tuple with `None` values added.
+
+    Raises:
+        TypeError: If the input is not a tuple.
+        ValueError: If the tuple is longer than the target length.
+    """
     if not isinstance(t, tuple):
         raise TypeError(f"Wrong type provided. Expected tuple, got {type(t)} : {t!r}")
     if len(t) > target_length:
@@ -24,21 +38,36 @@ def extend_tuple(t, target_length: int):
     return t + (None,) * (target_length - len(t))
 
 
-def extend_tuples_with_none(list_of_tuples: list[tuple], target_length: int):
-    """Extend the tuples in a list of tuples with None values to match target length."""
+def extend_tuples_with_none(list_of_tuples: List[tuple], target_length: int) -> List[tuple]:
+    """
+    Extend the tuples in a list of tuples with `None` values to match the target length.
+
+    Args:
+        list_of_tuples (List[tuple]): List of tuples to extend.
+        target_length (int): The target length for each tuple.
+
+    Returns:
+        List[tuple]: List of extended tuples.
+    """
     extended_tuples = []
     for tuple_ in list_of_tuples:
-        # if len(tuple_) > target_length:
-        #     raise ValueError(f"tuple is too long: {len(tuple_)}")
         extended_tuple = extend_tuple(tuple_, target_length)
         extended_tuples.append(extended_tuple)
     return extended_tuples
 
 
-def add_record(data: dict, tag: str, record) -> dict:
-    """Add tag and record to data dict.
+def add_record(data: dict, tag: str, record: Any) -> dict:
+    """
+    Add a record to a dictionary under a given tag.
 
-    The record can be many things."""
+    Args:
+        data (dict): The data dictionary to update.
+        tag (str): The tag under which the record should be added.
+        record (Any): The record to add.
+
+    Returns:
+        dict: The updated dictionary.
+    """
     if tag in data:
         if isinstance(data[tag], list):
             data[tag].append(record)
@@ -50,14 +79,20 @@ def add_record(data: dict, tag: str, record) -> dict:
 
 
 def read_xml_record(element: ET.Element) -> dict:
-    """Read entire record in a nested dict structure."""
+    """
+    Recursively read an XML element into a nested dictionary structure.
+
+    Args:
+        element (ET.Element): The XML element to read.
+
+    Returns:
+        dict: A dictionary representing the element and its children.
+    """
     data = dict()
     if element.attrib:
         data.update(element.attrib)
     for child in element:
         if len(child) > 1 and child.tag:
-            # if there is a list, process each element one by one
-            # either nested or a plain text
             data[child.tag] = [
                 add_record(
                     dict(),
@@ -67,19 +102,25 @@ def read_xml_record(element: ET.Element) -> dict:
                 for child in child
             ]
         elif child.text and child.text.strip():
-            # just plain text record
             data = add_record(data=data, tag=child.tag, record=child.text.strip())
         else:
             record = read_xml_record(child)
             data = add_record(data, child.tag, record)
     if not data:
-        # empty strings and None are normalzied to None
         return None
     return data
 
 
 def read_file(file: str) -> dict:
-    """Read all entries in a MaxQuant xml file."""
+    """
+    Read all entries in a MaxQuant XML file.
+
+    Args:
+        file (str): The file path of the XML file.
+
+    Returns:
+        dict: A dictionary representing the parsed XML content.
+    """
     tree: ET.ElementTree = ET.parse(file)
     root: ET.Element = tree.getroot()
     params: dict = read_xml_record(root)
@@ -87,21 +128,16 @@ def read_file(file: str) -> dict:
 
 
 def flatten_dict_of_dicts(d: dict, parent_key: str = "") -> dict:
-    """Build tuples for nested dictionaries for use as `pandas.MultiIndex`.
-
-    Parameters
-    ----------
-    d : dict
-        Nested dictionary for which all keys are flattened to tuples.
-    parent_key : str, optional
-        Outer key (used for recursion), by default ''
-
-    Returns
-    -------
-    dict
-        Flattend dictionary with tuple keys: {(outer_key, ..., inner_key) : value}
     """
-    # simplified and adapted from: https://stackoverflow.com/a/6027615/9684872
+    Flatten a nested dictionary to a single-level dictionary with tuple keys.
+
+    Args:
+        d (dict): The nested dictionary to flatten.
+        parent_key (str, optional): The outer key used for recursion, defaults to an empty string.
+
+    Returns:
+        dict: A flattened dictionary with tuple keys.
+    """
     items = []
     for k, v in d.items():
         new_key = parent_key + (k,) if parent_key else (k,)
@@ -120,79 +156,100 @@ def flatten_dict_of_dicts(d: dict, parent_key: str = "") -> dict:
     return items
 
 
-def build_Series_from_records(records, index_length=4):
+def build_Series_from_records(records: Any, index_length: int = 4) -> pd.Series:
+    """
+    Convert records into a pandas Series, using a MultiIndex.
+
+    Args:
+        records (Any): The records to convert.
+        index_length (int, optional): The length of the index. Defaults to 4.
+
+    Returns:
+        pd.Series: A pandas Series with the flattened records and a MultiIndex.
+    """
     records = flatten_dict_of_dicts(records)
     idx = pd.MultiIndex.from_tuples((extend_tuple(k, index_length) for (k, _) in records))
     return pd.Series((v for (k, v) in records), index=idx)
 
 
-def extract_params(fname, ms2frac="FTMS") -> ProteoBenchParameters:
+def extract_params(fname: str, ms2frac: str = "FTMS") -> ProteoBenchParameters:
+    """
+    Parse MaxQuant mqpar.xml file and extract relevant parameters.
+
+    Args:
+        fname (str): The file path to the MaxQuant parameter file.
+        ms2frac (str, optional): The MS2 fragmentation method to select. Defaults to "FTMS".
+
+    Returns:
+        ProteoBenchParameters: The extracted parameters as a `ProteoBenchParameters` object.
+    """
     params = ProteoBenchParameters()
 
     record = read_file(fname)
-    # select ms2 fragmentation method specified by parameter
-    # MaxQuant does this to our knowledge based on the binary rawfile metadata
+
+    # Select MS2 fragmentation method specified by parameter
     record["msmsParamsArray"] = [d for d in record["msmsParamsArray"] if d["msmsParams"]["Name"] == ms2frac]
     record = build_Series_from_records(record, 4).sort_index()
+
+    # Extract parameters from the XML record
     params.search_engine = "Andromeda"
     params.software_version = record.loc["maxQuantVersion"].squeeze()
     params.ident_fdr_psm = None
     params.ident_fdr_peptide = record.loc["peptideFdr"].squeeze()
     params.ident_fdr_protein = record.loc["proteinFdr"].squeeze()
     params.enable_match_between_runs = record.loc["matchBetweenRuns"].squeeze()
+
     precursor_mass_tolerance = record.loc[
         pd.IndexSlice["parameterGroups", "parameterGroup", "mainSearchTol", :]
     ].squeeze()
     params.precursor_mass_tolerance = f"{precursor_mass_tolerance} ppm"
-    # ! differences between version >1.6 and <=1.5
+
     fragment_mass_tolerance = record.loc[pd.IndexSlice["msmsParamsArray", "msmsParams", "MatchTolerance", :]].squeeze()
     in_ppm = bool(record.loc[pd.IndexSlice["msmsParamsArray", "msmsParams", "MatchToleranceInPpm", :]].squeeze())
     if in_ppm:
         fragment_mass_tolerance = f"{fragment_mass_tolerance} ppm"
     params.fragment_mass_tolerance = fragment_mass_tolerance
+
+    # Extract enzyme and cleavage settings
     params.enzyme = record.loc[("parameterGroups", "parameterGroup", "enzymes", "string")].squeeze()
     params.allowed_miscleavages = record.loc[
         pd.IndexSlice["parameterGroups", "parameterGroup", "maxMissedCleavages", :]
     ].squeeze()
+
+    # Extract peptide length information
     try:
         params.min_peptide_length = record.loc["minPepLen"].squeeze()
     except KeyError:
-        # Version 2.6 and above
         params.minPeptideLength = record.loc["minPeptideLength"].squeeze()
-    # minPeptideLengthForUnspecificSearch (what is it?)
-    params.max_peptide_length = None
-    # fixed mods
-    if params.software_version > "1.6.0.0":
-        fixed_mods = record.loc[pd.IndexSlice["parameterGroups", "parameterGroup", "fixedModifications", :]].squeeze()
-        if isinstance(fixed_mods, str):
-            params.fixed_mods = fixed_mods
-        else:
-            params.fixed_mods = ",".join(fixed_mods)
+
+    # Fixed and variable modifications
+    fixed_mods = record.loc[pd.IndexSlice["parameterGroups", "parameterGroup", "fixedModifications", :]].squeeze()
+    if isinstance(fixed_mods, str):
+        params.fixed_mods = fixed_mods
     else:
-        fixed_mods = record.loc[pd.IndexSlice["fixedModifications", :]].squeeze()
-        if isinstance(fixed_mods, str):
-            params.fixed_mods = fixed_mods
-        else:
-            params.fixed_mods = ",".join(fixed_mods)
+        params.fixed_mods = ",".join(fixed_mods)
 
     variable_mods = record.loc[pd.IndexSlice["parameterGroups", "parameterGroup", "variableModifications", :]].squeeze()
     if isinstance(variable_mods, str):
         params.variable_mods = variable_mods
     else:
         params.variable_mods = ",".join(variable_mods)
+
     params.max_mods = record.loc[("parameterGroups", "parameterGroup", "maxNmods")].squeeze()
     params.min_precursor_charge = None
     params.max_precursor_charge = record.loc[
         pd.IndexSlice["parameterGroups", "parameterGroup", "maxCharge", :]
     ].squeeze()
+
     return params
 
 
-# create a first version of json files to match
+# Create a first version of json files to match
 if __name__ == "__main__":
     import json
     from pprint import pprint
 
+    # Process different MaxQuant XML files
     for test_file in [
         "../../../test/params/mqpar_MQ1.6.3.3_MBR.xml",
         "../../../test/params/mqpar_MQ2.1.3.0_noMBR.xml",
@@ -201,23 +258,21 @@ if __name__ == "__main__":
     ]:
         print(f"{test_file = }")
         record = read_file(test_file)
-        (
-            Path(test_file)
-            .with_suffix(".json")
-            .write_text(
-                json.dumps(
-                    record,
-                    indent=4,
-                )
-            )
-        )
+
+        # Save the XML record as a JSON file
+        Path(test_file).with_suffix(".json").write_text(json.dumps(record, indent=4))
+
+        # Create a pandas Series from the record and save it as a CSV file
         record = build_Series_from_records(record, 4)
         record = record.to_frame("run_identifier")
         record.to_csv(Path(test_file).with_suffix(".csv"))
+
+        # Extract parameters and save them in JSON format
         params = extract_params(test_file, ms2frac="FTMS")
         pprint(params.__dict__)
+
+        # Save extracted parameters as a JSON file
         test_file = Path(test_file)
         fname = Path(str(test_file.with_suffix(".json").with_name(test_file.stem + "_sel")) + ".json")
-
         with open(fname, "w") as f:
             json.dump(params.__dict__, f, indent=4)

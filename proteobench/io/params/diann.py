bd080d5c84b791d42262fa3e69065bf6c9a64da0
RobbinBouwmeester <robbin.bouwmeester@ugent.be>
2024-12-02 16:23:11 +0100

    Revert diann - new fragger

--- proteobench/io/params/diann.py
+++ proteobench/io/params/diann.py
@@ -1,17 +1,15 @@
 import pathlib
 import re
-from typing import Any, Dict, List, Optional
+from typing import Any, List, Optional
 
 import pandas as pd
 
 from proteobench.io.params import ProteoBenchParameters
 
-# Regular expression patterns for extracting specific settings
 mass_tolerance_regex = r"(?<=Optimised mass accuracy: )\d*\.?\d+(?= ppm)"
 software_version_regex = r"(?<=DIA-NN\s)(.*?)(?=\s\(Data-Independent Acquisition by Neural Networks\))"
 scan_window_regex = r"(?<=Scan window radius set to )\d+"
 
-# Dictionary to map command-line settings to ProteoBench parameters
 PARAM_CMD_DICT = {
     "ident_fdr_peptide": "qvalue",
     "ident_fdr_protein": "qvalue",
@@ -29,8 +27,6 @@ PARAM_CMD_DICT = {
     "max_precursor_charge": "max-pr-charge",
     "scan_window": "window",
 }
-
-# Lists of settings that should be treated as floats, integers, or modifications
 SETTINGS_PB_FLOAT = [
     "ident_fdr_psm",
     "ident_fdr_peptide",
@@ -54,11 +50,17 @@ def find_cmdline_string(lines: List[str]) -> Optional[str]:
     """
     Find the command line statement in the log file of DIANN.
 
-    Args:
-        lines (List[str]): All input lines from the DIA-NN log file.
+    It is assumed that this statement is stored on a single line.
 
-    Returns:
-        Optional[str]: The command line string if found, otherwise None.
+    Parameter
+    ---------
+    lines: list[str]
+        All input lines from the DIA-NN log file.
+
+    Return
+    ------
+    str
+        The command line string.
     """
     for line in lines:
         if "diann" in line and "--f" in line and "--fasta" in line:
@@ -68,15 +70,21 @@ def find_cmdline_string(lines: List[str]) -> Optional[str]:
 
 def parse_cmdline_string(line: str) -> dict:
     """
-    Parse the command line string to extract settings.
+    Parse the command line string to the settings it specifies.
 
-    Args:
-        line (str): The command line string to parse.
+    The GitHub README.md of DIA-NN from version 1.9 was used to interpret settings.
 
-    Returns:
-        dict: A dictionary of parsed settings.
-            - Keys are setting names.
-            - Values are lists of settings (or booleans if the setting is a flag).
+    Parameter
+    ---------
+    line: str
+        The command line string to parse.
+
+    Return
+    ------
+    dict:
+        Parsed setting parameters in dictionary format.
+        Keys are setting names and values the inputted setting in list format.
+        The value is boolean if the setting is considered a boolean flag.
     """
     setting_dict = {}
     settings = [setting.split() for setting in line.split(" --")]
@@ -94,21 +102,26 @@ def parse_cmdline_string(line: str) -> dict:
             setting_dict[setting_list[0]] = setting_list[1:]
 
     setting_dict["var-mod"] = var_mods
-    if "mod" not in setting_dict:
+    if "mod" not in setting_dict.keys():
         setting_dict["mod"] = fixed_mods
     return setting_dict
 
 
 def parse_setting(setting_name: str, setting_list: list) -> Any:
     """
-    Parse individual settings based on their type.
+    Parse individual settings based on their setting type.
 
-    Args:
-        setting_name (str): The name of the setting.
-        setting_list (list): The list of values for the setting.
+    Parameters
+    ----------
+    setting_name: str
+        The name of the setting (ProteoBench).
+    setting_list: list
+        The input value of a given setting.
 
-    Returns:
-        Any: The parsed value, which could be a float, integer, or string.
+    Return
+    ------
+    Any
+        The parsed setting.
     """
     if setting_name in SETTINGS_PB_FLOAT:
         assert len(setting_list) == 1
@@ -121,100 +134,121 @@ def parse_setting(setting_name: str, setting_list: list) -> Any:
     return "".join(setting_list)
 
 
-def extract_with_regex(lines: List[str], regex: str) -> str:
+def extract_with_regex(lines: List[str], regex) -> str:
     """
-    Extract a value from lines using the provided regular expression.
+    If no mass accuracy was specified in the cmd string, extract it from the log-file.
 
-    Args:
-        lines (List[str]): All input lines from the DIA-NN log file.
-        regex (str): The regular expression pattern to search for.
+    Parameter
+    ---------
+    lines: list[str]
+        All input lines from the DIA-NN log file.
 
-    Returns:
-        str: The matched string if found, otherwise None.
+    Return
+    ------
+    str:
+        The MS1 and MS2 mass accuracy specified in ppm.
     """
     for line in lines:
         regex_match = re.search(regex, line)
         if regex_match:
-            return regex_match.group(0)
+            x = regex_match.group(0)
+            return x
     return None
 
 
 def parse_protein_inference_method(cmdline_dict: dict) -> str:
     """
-    Parse the protein inference method from the command-line settings.
+    Parse the protein inference method from the parsed execution command string.
 
-    Args:
-        cmdline_dict (dict): The parsed command line settings.
+    This setting is defined by disparate setting tags, namely:
+    - no-prot-inf: No protein inference
+    - pg-level: Code specifies inference method
 
-    Returns:
-        str: The protein inference method.
-            Possibilities are:
-            - "Disabled"
-            - "Isoforms"
-            - "Protein_names"
-            - "Genes"
+    Parameter
+    ---------
+    cmdline_dict: dict
+        Parsed execution command string
+
+    Return
+    ------
+    str
+        The protein inference method.
+        Possibilities:
+        - Disabled
+        - Isoforms
+        - Protein_names
+        - Genes
     """
-    if "no-prot-inf" in cmdline_dict:
+    if "no-prot-inf" in cmdline_dict.keys():
         return "Disabled"
-    elif "pg-level" in cmdline_dict:
+    elif "pg-level" in cmdline_dict.keys():
         pg_setting = cmdline_dict["pg-level"][0]
         pg_level_mapping = {"0": "Isoforms", "1": "Protein_names", "2": "Genes"}
         try:
             return pg_level_mapping[pg_setting]
         except KeyError:
-            raise Exception(f"Unexpected setting passed to --pg-level: {pg_setting}")
+            Exception(f"Unexpected setting passed to --pg-level in diann.exe: {pg_setting}")
 
 
-def parse_quantification_strategy(cmdline_dict: dict) -> str:
+def parse_quantification_strategy(cmdline_dict: dict):
     """
-    Parse the quantification strategy from the command-line settings.
+    Parse the quatnification method from the parsed execution command string.
 
-    Args:
-        cmdline_dict (dict): The parsed command line settings.
+    This setting is defined by disparate setting tags, namely:
+    - direct-quant: use legacy quantification within DIANN
+    - high-acc: QuantUMS high-accuracy setting
+    - no tag: Default is QuantUMS high-precision
 
-    Returns:
-        str: The quantification method.
-            Possibilities are:
-            - "Legacy"
-            - "QuantUMS high-accuracy"
-            - "QuantUMS high-precision"
+    Parameter
+    ---------
+    cmdline_dict: dict
+        Parsed execution command string
+
+    Return
+    ------
+    str
+        The quantification method.
+        Possibilities:
+        - Legacy
+        - QuantUMS high-accuracy
+        - QuantUMS high-precision
     """
-    if "direct-quant" in cmdline_dict:
+    if "direct-quant" in cmdline_dict.keys():
         return "Legacy"
-    elif "high-acc" in cmdline_dict:
+    elif "high-acc" in cmdline_dict.keys():
         return "QuantUMS high-accuracy"
     else:
-        return "QuantUMS high-precision"  # Default value
+        # Default value
+        return "QuantUMS high-precision"
 
 
-def parse_predictors_library(cmdline_dict: dict) -> Dict[str, str]:
+def parse_predictors_library(cmdline_dict: dict):
     """
-    Parse the spectral library predictors from the parsed execution command string.
+    Parse the spectral library predictors from parsed execute command string.
 
-    Args:
-        cmdline_dict (dict): The parsed command line settings.
+    For now, only 'DIANN' and 'User defined speclib' are supported.
+    In the future, the user might specify which algorithm was used for library generation.
 
-    Returns:
-        dict: A dictionary specifying the algorithm used for RT, IM, and MS2_int predictions.
+    Parameter
+    ---------
+    cmdline_dict: dict
+        Parsed execution command string
+
+    Return
+    ------
+    dict
+        Dictionary specifying algorithm name for RT, IM and MS2_int.
     """
-    if "predictor" in cmdline_dict:
+    if "predictor" in cmdline_dict.keys():
         return {"RT": "DIANN", "IM": "DIANN", "MS2_int": "DIANN"}
-    elif "lib" in cmdline_dict:
+    elif "lib" in cmdline_dict.keys():
         if not isinstance(cmdline_dict["lib"], bool):
             return {"RT": "User defined speclib", "IM": "User defined speclib", "MS2_int": "User defined speclib"}
 
 
 def extract_params(fname: str) -> ProteoBenchParameters:
-    """
-    Parse the DIA-NN log file and extract relevant parameters into a ProteoBenchParameters object.
-
-    Args:
-        fname (str): The path to the DIA-NN log file.
-
-    Returns:
-        ProteoBenchParameters: A parameters object containing extracted settings.
-    """
-    # Default parameters
+    """Parse DIA-NN log file and extract relevant parameters."""
+    # Some default and flag settings
     parameters = {
         "software_name": "DIA-NN",
         "search_engine": "DIA-NN",
@@ -224,40 +258,44 @@ def extract_params(fname: str) -> ProteoBenchParameters:
     }
 
     try:
+        # Read in the log file
         with open(fname) as f:
             lines = f.readlines()
     except:
         lines = [l for l in fname.read().decode("utf-8").splitlines()]
 
-    # Extract software version
-    software_version = extract_with_regex(lines, software_version_regex)
+    # Extract software versions from the log file.
+    software_version = search_engine_version = extract_with_regex(lines, software_version_regex)
     parameters["software_version"] = software_version
-    parameters["search_engine_version"] = software_version
+    parameters["search_engine_version"] = search_engine_version
 
-    # Extract settings from command-line
+    # Get settings from the execution command string
     cmdline_string = find_cmdline_string(lines)
     cmdline_dict = parse_cmdline_string(cmdline_string)
 
-    parameters["second_pass"] = "double-search" in cmdline_dict or "double-pass" in cmdline_dict
+    parameters["second_pass"] = "double-search" in cmdline_dict.keys() or "double-pass" in cmdline_dict.keys()
     parameters["quantification_method"] = parse_quantification_strategy(cmdline_dict)
     parameters["protein_inference"] = parse_protein_inference_method(cmdline_dict)
     parameters["predictors_library"] = parse_predictors_library(cmdline_dict)
 
-    # Map settings from command line to ProteoBench parameters
+    # Parse most settings as possible from the execution command using PARAM_CMD_DICT for mapping.
     for proteobench_setting, cmd_setting in PARAM_CMD_DICT.items():
-        if cmd_setting in cmdline_dict:
+        if cmd_setting in cmdline_dict.keys():
             if isinstance(cmdline_dict[cmd_setting], bool):
                 parameters[proteobench_setting] = cmdline_dict[cmd_setting]
             else:
                 parameters[proteobench_setting] = parse_setting(proteobench_setting, cmdline_dict[cmd_setting])
 
-    # Extract mass tolerance if not present in command line
-    if "precursor_mass_tolerance" not in parameters:
+    # If mass-acc flag is not present in cmdline string, extract it from the log file
+    if "precursor_mass_tolerance" not in parameters.keys():
         mass_tol = extract_with_regex(lines, mass_tolerance_regex)
-        parameters["precursor_mass_tolerance"] = f"{mass_tol} ppm"
-        parameters["fragment_mass_tolerance"] = f"{mass_tol} ppm"
+        parameters["precursor_mass_tolerance"] = mass_tol + " ppm"
+        parameters["fragment_mass_tolerance"] = mass_tol + " ppm"
+    else:
+        parameters["precursor_mass_tolerance"] = str(parameters["precursor_mass_tolerance"]) + " ppm"
+        parameters["fragment_mass_tolerance"] = str(parameters["fragment_mass_tolerance"]) + " ppm"
 
-    # Extract scan window
+    # If scan window is not customely set, extract it from the log file
     parameters["scan_window"] = int(extract_with_regex(lines, scan_window_regex))
 
     return ProteoBenchParameters(**parameters)
--- proteobench/io/params/fragger.py
+++ proteobench/io/params/fragger.py
@@ -1,6 +1,6 @@
 """Functionality to parse FragPipe fragger.params parameter files.
 
-FragPipe has a text based paramter file format which
+FragPipe has a text based parameter file format which
 separates parameters and their value using an equal sign. Optional comments are
 expressed with a hash sign.
 """
@@ -12,6 +12,7 @@ import re
 from collections import namedtuple
 from io import BytesIO
 from pathlib import PureWindowsPath
+from typing import List, Optional, Tuple
 
 import pandas as pd
 
@@ -24,18 +25,26 @@ Parameter = namedtuple("Parameter", ["name", "value", "comment"])
 VERSION_NO_PATTERN = r"\d+(\.\d+)*"
 
 
-def parse_params(l_of_str: list[str], sep: str = " = ") -> list[Parameter]:
-    """Read FragPipe parameter file as list of records."""
+def parse_params(l_of_str: List[str], sep: str = " = ") -> List[Parameter]:
+    """
+    Parse the FragPipe parameter file and return a list of Parameter objects.
+
+    Args:
+        l_of_str (List[str]): The lines of the FragPipe parameter file as a list of strings.
+        sep (str): The separator between parameter names and values. Default is " = ".
+
+    Returns:
+        List[Parameter]: A list of Parameter namedtuples containing the parameter name, value, and any comment.
+    """
     data = []
     for line in l_of_str:
         line = line.strip()
         logger.debug(line)
-        # ! logic below also allows to keep the comments as comments
         if line.startswith("#"):
-            continue
+            continue  # Skip comments
         if not line:
-            continue
-        if "#" in line:
+            continue  # Skip empty lines
+        if "#" in line:  # Handle lines with inline comments
             res = line.split("#")
             if len(res) == 1:
                 comment = res[0]
@@ -55,64 +64,89 @@ def parse_params(l_of_str: list[str], sep: str = " = ") -> list[Parameter]:
     return data
 
 
-def read_fragpipe_workflow(file: BytesIO, sep: str = "=") -> list[Parameter]:
+def read_fragpipe_workflow(file: BytesIO, sep: str = "=") -> Tuple[str, List[Parameter]]:
+    """
+    Reads the FragPipe workflow file, extracting the header and parameters.
+
+    Args:
+        file (BytesIO): The FragPipe workflow file to read.
+        sep (str): The separator used between parameter names and values. Default is "=".
+
+    Returns:
+        Tuple[str, List[Parameter]]: A tuple containing the header and a list of Parameter objects.
+    """
     l_of_str = file.read().decode("utf-8").splitlines()
-    header = l_of_str[0][1:].strip()
+    header = l_of_str[0][1:].strip()  # Skip leading '#' in the header
     return header, parse_params(l_of_str, sep=sep)
 
 
 def extract_params(file: BytesIO) -> ProteoBenchParameters:
-    """Parse FragPipe parameter files and extract relevant parameters."""
+    """
+    Parse FragPipe parameter files and extract relevant parameters into a `ProteoBenchParameters` object.
+
+    Args:
+        file (BytesIO): The FragPipe parameter file to parse.
+
+    Returns:
+        ProteoBenchParameters: The extracted parameters encapsulated in a ProteoBenchParameters object.
+    """
     header, fragpipe_params = read_fragpipe_workflow(file)
     fragpipe_params = pd.DataFrame.from_records(fragpipe_params, columns=Parameter._fields).set_index(
         Parameter._fields[0]
     )["value"]
 
+    # Extract version from header
     match = re.search(VERSION_NO_PATTERN, header)
-
     if match:
         header = match.group()
 
+    # Initialize ProteoBenchParameters
     params = ProteoBenchParameters()
     params.software_name = "FragPipe"
     params.software_version = header
     params.search_engine = "MSFragger"
 
     try:
+        # Extract MSFragger executable version
         msfragger_executable = fragpipe_params.loc["fragpipe-config.bin-msfragger"]
         msfragger_executable = PureWindowsPath(msfragger_executable).name
         match = re.search(VERSION_NO_PATTERN, msfragger_executable)
-
         if match:
             msfragger_executable = match.group()
     except KeyError:
         msfragger_executable = ""
 
     params.search_engine_version = msfragger_executable
+
+    # Enzyme and cleavage settings
     enzyme = fragpipe_params.loc["msfragger.search_enzyme_name_1"]
     if fragpipe_params.loc["msfragger.search_enzyme_name_2"] != "null":
         enzyme += f"|{fragpipe_params.loc['msfragger.search_enzyme_name_2']}"
     params.enzyme = enzyme
     params.allowed_miscleavages = fragpipe_params.loc["msfragger.allowed_missed_cleavage_1"]
-    # TODO: Fragpipe reports "0.0" mass shift for each unmodified AA here, which is not useful and could be removed
+
+    # Modifications
     params.fixed_mods = fragpipe_params.loc["msfragger.table.fix-mods"]
-    # TODO: Fragpipe reports a lot of default suggestions for variable mods and assigns them as 'false' here,
-    # which is not useful and could be removed, i.e. only retain true variable mods
     params.variable_mods = fragpipe_params.loc["msfragger.table.var-mods"]
     params.max_mods = fragpipe_params.loc["msfragger.max_variable_mods_per_peptide"]
+
+    # Peptide length
     params.min_peptide_length = fragpipe_params.loc["msfragger.digest_min_length"]
     params.max_peptide_length = fragpipe_params.loc["msfragger.digest_max_length"]
 
+    # Precursor mass tolerance
     precursor_mass_units = "Da"
     if int(fragpipe_params.loc["msfragger.precursor_mass_units"]):
         precursor_mass_units = "ppm"
     params.precursor_mass_tolerance = f'{fragpipe_params.loc["msfragger.precursor_mass_lower"]} {precursor_mass_units}|{fragpipe_params.loc["msfragger.precursor_mass_upper"]} {precursor_mass_units}'
 
+    # Fragment mass tolerance
     fragment_mass_units = "Da"
     if int(fragpipe_params.loc["msfragger.fragment_mass_units"]):
         fragment_mass_units = "ppm"
     params.fragment_mass_tolerance = f'{fragpipe_params.loc["msfragger.fragment_mass_tolerance"]} {fragment_mass_units}'
-    # ! ionquant is not necessarily fixed?
+
+    # Quantification settings
     if fragpipe_params.loc["quantitation.run-label-free-quant"] == "true":
         params.ident_fdr_protein = fragpipe_params.loc["ionquant.proteinfdr"]
         params.ident_fdr_peptide = fragpipe_params.loc["ionquant.peptidefdr"]
@@ -122,21 +156,15 @@ def extract_params(file: BytesIO) -> ProteoBenchParameters:
         params.ident_fdr_peptide = fragpipe_params.loc["diann.q-value"]
         params.ident_fdr_psm = fragpipe_params.loc["diann.q-value"]
 
-    # I think this is incorrect? The values are stored as proportions in the fragpipe.workflow file? Commenting out for now
-    # for key in ["ident_fdr_protein", "ident_fdr_peptide", "ident_fdr_psm"]:
-    #     value = getattr(params, key)
-    #     try:
-    #         value = int(value) / 100
-    #         setattr(params, key, value)
-    #     except ValueError:
-    #         logging.warning(f"Could not convert {value} to int.")
-
+    # Precursor charge settings
     if fragpipe_params.loc["msfragger.override_charge"] == "true":
         params.min_precursor_charge = int(fragpipe_params.loc["msfragger.misc.fragger.precursor-charge-lo"])
         params.max_precursor_charge = int(fragpipe_params.loc["msfragger.misc.fragger.precursor-charge-hi"])
-    else:  # Fragpipe takes charge info from data, this is the default
+    else:
         params.min_precursor_charge = 1
         params.max_precursor_charge = None
+
+    # Match between runs and quantification method settings
     if fragpipe_params.loc["quantitation.run-label-free-quant"] == "true":
         params.enable_match_between_runs = bool(fragpipe_params.loc["ionquant.mbr"])
     elif fragpipe_params.loc["diann.run-dia-nn"] == "true":
@@ -151,46 +179,30 @@ def extract_params(file: BytesIO) -> ProteoBenchParameters:
         else:
             params.enable_match_between_runs = False
         params.quantification_method = diann_quant_dict[int(fragpipe_params.loc["diann.quantification-strategy"])]
+
+    # Protein inference settings
     if fragpipe_params.loc["protein-prophet.run-protein-prophet"] == "true":
-        params.protein_inference = "ProteinProphet: {}".format(fragpipe_params.loc["protein-prophet.cmd-opts"])
+        params.protein_inference = f"ProteinProphet: {fragpipe_params.loc['protein-prophet.cmd-opts']}"
 
     return params
 
 
 if __name__ == "__main__":
-    # TODO create test csv files
-    import pathlib
-    from pprint import pprint
+    # Process FragPipe workflow file and extract parameters
+    files = [
+        "../../../test/params/fragpipe.workflow",
+        "../../../test/params/fragpipe_win_paths.workflow",
+        "../../../test/params/fragpipe_v22.workflow",
+    ]
 
-    file = pathlib.Path("../../../test/params/fragpipe.workflow")
-    with open(file, "rb") as f:
-        _, data = read_fragpipe_workflow(f)
-    df = pd.DataFrame.from_records(data, columns=Parameter._fields).set_index(Parameter._fields[0])
-    df.to_csv(file.with_suffix(".csv"))
-    with open(file, "rb") as f:
-        params = extract_params(f)
-    pprint(params.__dict__)
-    series = pd.Series(params.__dict__)
-    series.to_csv(file.parent / f"{file.stem}_extracted_params.csv")
-
-    file = pathlib.Path("../../../test/params/fragpipe_win_paths.workflow")
-    with open(file, "rb") as f:
-        _, data = read_fragpipe_workflow(f)
-    df = pd.DataFrame.from_records(data, columns=Parameter._fields).set_index(Parameter._fields[0])
-    df.to_csv(file.with_suffix(".csv"))
-    with open(file, "rb") as f:
-        params = extract_params(f)
-    pprint(params.__dict__)
-    series = pd.Series(params.__dict__)
-    series.to_csv(file.parent / f"{file.stem}_extracted_params.csv")
-
-    file = pathlib.Path("../../../test/params/fragpipe_v22.workflow")
-    with open(file, "rb") as f:
-        _, data = read_fragpipe_workflow(f)
-    df = pd.DataFrame.from_records(data, columns=Parameter._fields).set_index(Parameter._fields[0])
-    df.to_csv(file.with_suffix(".csv"))
-    with open(file, "rb") as f:
-        params = extract_params(f)
-    pprint(params.__dict__)
-    series = pd.Series(params.__dict__)
-    series.to_csv(file.parent / f"{file.stem}_extracted_params.csv")
+    for file_path in files:
+        file = pathlib.Path(file_path)
+        with open(file, "rb") as f:
+            _, data = read_fragpipe_workflow(f)
+        df = pd.DataFrame.from_records(data, columns=Parameter._fields).set_index(Parameter._fields[0])
+        df.to_csv(file.with_suffix(".csv"))
+        with open(file, "rb") as f:
+            params = extract_params(f)
+        pprint(params.__dict__)
+        series = pd.Series(params.__dict__)
+        series.to_csv(file.parent / f"{file.stem}_extracted_params.csv")

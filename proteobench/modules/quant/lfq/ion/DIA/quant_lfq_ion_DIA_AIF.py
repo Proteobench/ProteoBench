71bb765c7ac2d11f012aa2ea10a03b42d9d6c3da
RobbinBouwmeester <robbin.bouwmeester@ugent.be>
2024-12-03 10:16:21 +0100

    update diapasef

--- proteobench/modules/quant/lfq/ion/DIA/quant_lfq_ion_DIA_AIF.py
+++ proteobench/modules/quant/lfq/ion/DIA/quant_lfq_ion_DIA_AIF.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import os
-from typing import Any, Dict, Optional, Tuple
+from typing import Optional, Tuple
 
 import pandas as pd
 from pandas import DataFrame

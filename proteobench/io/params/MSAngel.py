54e3e31d8dc3ec089c86e9067730f16381ec782f
RobbinBouwmeester <robbin.bouwmeester@ugent.be>
2024-12-11 17:07:34 +0100

    Update MSAngel.py

--- proteobench/io/params/MSAngel.py
+++ proteobench/io/params/MSAngel.py
@@ -17,21 +17,23 @@ import pandas as pd
 
 from proteobench.io.params import ProteoBenchParameters
 
+
 def extract_search_engine(search_params: list) -> dict:
     """
     Extract search engine parameters from the JSON data.
-    The parameter format depends on the search engine used, so this functino needs to be 
+    The parameter format depends on the search engine used, so this functino needs to be
     updated for each search engine. Currently, it is set up for:
     . Mascot
     """
 
     all_search_engines = []
-    for each_search_params in search_params['operations']:
-     print('1')
-     if 'searchEnginesWithForms' in each_search_params:
-        all_search_engines.append(each_search_params['searchEnginesWithForms'][0][0])
-    
+    for each_search_params in search_params["operations"]:
+        print("1")
+        if "searchEnginesWithForms" in each_search_params:
+            all_search_engines.append(each_search_params["searchEnginesWithForms"][0][0])
+
     return all_search_engines
+
 
 def extract_params(fname: Union[str, pathlib.Path]) -> ProteoBenchParameters:
     """
@@ -60,12 +62,16 @@ def extract_params(fname: Union[str, pathlib.Path]) -> ProteoBenchParameters:
 
     ## Extract the search engine(s) parameters before concatenating them:
     all_search_engines = extract_search_engine(data)
-    params.search_engines = all_search_engines.join(',')
+    params.search_engines = all_search_engines.join(",")
     all_search_engines = []
     all_enzyme = []
     all_allowed_miscleavages = []
     all_fixed_mods = []
     all_variable_mods = []
+
+    # TODO needs to have actual values
+    all_search_params = {}
+
     for key, value in all_search_params.items():
         all_search_engines.append(value["format"])
         all_enzyme.append(value["enzyme"]["cleave_at"])
@@ -73,7 +79,8 @@ def extract_params(fname: Union[str, pathlib.Path]) -> ProteoBenchParameters:
         all_fixed_mods.append(value["static_mods"])
         all_variable_mods.append(value["variable_mods"])
 
-    params.search_engine =
+    # TODO need to have an actual value
+    params.search_engine = ""
     params.search_engine_version = data["version"]
     params.enzyme = data["database"]["enzyme"]["cleave_at"]
     params.allowed_miscleavages = data["database"]["enzyme"]["missed_cleavages"]

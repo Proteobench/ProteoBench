84329d646631666edf9f8ffee5549c97d152ba02
RobbinBouwmeester <robbin.bouwmeester@ugent.be>
2025-01-21 16:56:08 +0100

    Change the rows based on json for tests

--- test/test_parse_params_alphapept.py
+++ test/test_parse_params_alphapept.py
@@ -21,4 +21,5 @@ def test_extract_params(file):
     actual = alpahpept_params.extract_params(file)
     actual = pd.Series(actual.__dict__)
     actual = pd.read_csv(io.StringIO(actual.to_csv()), index_col=0).squeeze("columns")
+    expected = expected.loc[actual.index]
     assert expected.equals(actual)
--- test/test_parse_params_peaks.py
+++ test/test_parse_params_peaks.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pandas as pd
 import pytest
+
 import proteobench.io.params.peaks as peaks_params
 
 TESTDATA_DIR = Path(__file__).parent / "params"
@@ -22,4 +23,5 @@ def test_read_peaks_settings(file):
     print(actual.software_name)
     actual = pd.Series(actual.__dict__)
     actual = pd.read_csv(io.StringIO(actual.to_csv()), index_col=0).squeeze("columns")
+    expected = expected.loc[actual.index]
     assert expected.equals(actual)

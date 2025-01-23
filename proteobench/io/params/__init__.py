4dd78b6a7a31a699414733c76ac555e2b72723a0
RobbinBouwmeester <robbin.bouwmeester@ugent.be>
2025-01-23 11:31:55 +0100

    Update __init__.py

--- proteobench/io/params/__init__.py
+++ proteobench/io/params/__init__.py
@@ -10,67 +10,6 @@ import numpy as np
 
 @dataclass
 class ProteoBenchParameters:
-    """
-    Parameters for a proteomics search engine.
-
-    Attributes
-    ----------
-    software_name : Optional[str]
-        Name of the software tool / pipeline used for this benchmark run
-        (examples: "MaxQuant", "AlphaPept", "Proline", ...).
-    software_version : Optional[str]
-        Version of the software tool / pipeline used for this benchmark run
-    search_engine: Optional[str]
-        Search engine used for this benchmark run
-        (examples: "Andromeda", "Mascot", ...).
-    search_engine_version : Optional[str]
-        Version of the search engine used for this benchmark run.
-    ident_fdr_psm : Optional[str]
-        False discovery rate (FDR) threshold for peptide-spectrum match
-        (PSM) validation ("0.01" = 1%).
-    ident_fdr_peptide : Optional[str]
-        False discovery rate (FDR) threshold for peptide validation ("0.01" = 1%).
-    ident_fdr_protein : Optional[str]
-        False discovery rate (FDR) threshold for protein validation ("0.01" = 1%).
-    enable_match_between_runs : Optional[bool]
-        Match between run (also named cross assignment) is enabled.
-    precursor_mass_tolerance : Optional[str]
-       Precursor mass tolerance used for the search.
-       Given as an interval of upper and lower tolerance, e.g. [-20 ppm, 20 ppm].
-    fragment_mass_tolerance : Optional[str]
-        Precursor mass tolerance used for the search:
-        Given as an interval of upper and lower tolerance, e.g. [-0.02 Da, 0.02 Da].
-    enzyme : Optional[str]
-        Enzyme used as parameter for the search. If several, use "|".
-    allowed_miscleavages : Optional[int]
-        Maximal number of missed cleavages allowed.
-    min_peptide_length : Optional[str]
-        Minimum peptide length (number of residues) allowed for the search.
-    max_peptide_length : Optional[str]
-        Maximum peptide length (number of residues) allowed for the search.
-    fixed_mods : Optional[str]
-        Fixed modifications searched for in the search. If several, separate with "|".
-    variable_mods : Optional[str]
-        Variable modifications searched for in the search. If several, separate with "|".
-    max_mods : Optional[int]
-        Maximal number of modifications per peptide
-        (including fixed and variable modifications).
-    min_precursor_charge : Optional[int]
-        Minimum precursor charge allowed.
-    max_precursor_charge : Optional[int]
-        Maximum precursor charge allowed.
-    spectral_library_generation : Optional[dict]
-        Models used to generate spectral library (DIA-specific).
-    scan_window : Optional[int]
-        Scan window radius. Ideally corresponds to approximate
-        average number of data points per peak (DIA-specific).
-    quantification_method_DIANN : Optional[str]
-        Quantification strategy used in the DIA-NN engine (DIANN-specific).
-    second_pass : Optional[bool]
-        Whether second pass search is enabled (DIANN-specific).
-    protein_inference : Optional[str]
-        Protein inference method used.
-    """
     def __init__(
         self, filename=os.path.join(os.path.dirname(__file__), "json/Quant/lfq/ion/DDA/fields.json"), **kwargs
     ):

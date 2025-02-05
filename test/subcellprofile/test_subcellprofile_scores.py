1ef68198ed4099957606140654136f1b4e7661b8
Julia Schessner <schessner@biochem.mpg.de>
2025-02-05 11:05:41 +0100

    add docstrings to tests

--- test/subcellprofile/test_subcellprofile_scores.py
+++ test/subcellprofile/test_subcellprofile_scores.py
@@ -7,8 +7,10 @@ from proteobench.score.subcellprofile.subcellprofile_scores import Subcellprofil
 
 dir = os.path.dirname(__file__)
 
+
 @fixture
 def sp_scores():
+    """Fixture to return a Subcellprofile_Scores object with valid data."""
     settings = os.path.join(dir, "../data/subcellprofile/domqc_settings_raw_input.json")
     content = os.path.join(dir, "../data/subcellprofile/pg.matrix.tsv")
     settings = json.load(open(settings))
@@ -16,8 +18,10 @@ def sp_scores():
     sp_scores = Subcellprofile_Scores()
     sp_scores.generate_SpatialDataSet(content, settings)
     return sp_scores
- 
+
+
 def test_generate_SpatialDataset():
+    """Test the SpatialDataSet is generated correctly and contains the dictionary with the right experiment name."""
     settings = os.path.join(dir, "../data/subcellprofile/domqc_settings_raw_input.json")
     content = os.path.join(dir, "../data/subcellprofile/pg.matrix.tsv")
     settings = json.load(open(settings))
@@ -26,5 +30,8 @@ def test_generate_SpatialDataset():
     sp_scores.generate_SpatialDataSet(content, settings)
     assert list(sp_scores.sd.analysed_datasets_dict.keys()) == ["AlphaDIA 1.9.2 Lumos_predicted"]
 
-def test_run_SpatialDataSetComparison(sp_scores):
+
+# TODO: Parameterize this for different input formats
+def test_run_SpatialDataSetComparison_noerrors(sp_scores: Subcellprofile_Scores):
+    """Running the SpatialDataSetComparison should not raise errors with any input."""
     sp_scores.run_SpatialDataSetComparison()

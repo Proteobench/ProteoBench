77059382382b5c94659f874aec77a2c66adbf3cb
David M. Hollenstein <hollenstein.david@gmail.com>
2025-03-01 15:53:41 +0100

    fix: Resolve relative paths to absolute

--- proteobench/modules/constants.py
+++ proteobench/modules/constants.py
@@ -1,7 +1,7 @@
 import pathlib
 
 IO_PARSE_SETTINGS_DIR = pathlib.Path(__file__) / ".." / ".." / "io" / "parsing" / "io_parse_settings"
-QUANT_LFQ_SETTINGS_DIR = IO_PARSE_SETTINGS_DIR / "Quant" / "lfq"
+QUANT_LFQ_SETTINGS_DIR = (IO_PARSE_SETTINGS_DIR / "Quant" / "lfq").resolve()
 
 MODULE_SETTINGS_DIRS = {
     "quant_lfq_ion_DIA_AIF": (QUANT_LFQ_SETTINGS_DIR / "ion" / "DIA" / "AIF").as_posix(),

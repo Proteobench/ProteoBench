d9f40464e38fd64d223e79b4885ee0c22b26951a
rodvrees <robbe.devreese@hotmail.com>
2025-03-10 15:38:10 +0100

    Remove write_csv line

--- proteobench/io/parsing/utils.py
+++ proteobench/io/parsing/utils.py
@@ -17,13 +17,11 @@ def add_fixed_mod(proforma: str, mod_name: str, aas: str) -> str:
 
 def add_maxquant_fixed_modifications(params: ProteoBenchParameters, result_perf: pd.DataFrame) -> pd.DataFrame:
     if hasattr(params, "fixed_mods"):
-        fixed_mods = getattr(params, "fixed_mods").split(",")
+        fixed_mods = params.fixed_mods.split(",")
 
         for mod in fixed_mods:
             mod_name, aas = mod.split(" ")
-            aas = [aa for aa in aas[1:-1]]
-            result_perf["precursor ion"] = result_perf["precursor ion"].apply(add_fixed_mod, args=(mod_name, aas))
+            aas_list = list(aas[1:-1])
+            result_perf["precursor ion"] = result_perf["precursor ion"].apply(add_fixed_mod, args=(mod_name, aas_list))
 
-    result_perf.to_csv("result_perf.csv", index=False)
-    # Overwrite the dataframes for submission
     return result_perf

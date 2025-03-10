c5fc642a29f3c7e9d054ed7a8f6b867d65cbf9d6
RobbinBouwmeester <robbin.bouwmeester@ugent.be>
2025-03-10 13:00:28 +0100

    Remove nesting

--- proteobench/modules/quant/lfq/ion/DDA/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Module for LFQ ion quantification using DDA data.
-"""
--- webinterface/pages/pages_variables/Quant/lfq/peptidoform/DDA/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Initializes the DDA peptidoform quantification page for the LFQ section of the ProteoBench web interface.
-"""

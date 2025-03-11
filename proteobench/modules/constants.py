826f1f6e09d9d7b0503afe6aea0a9598dbb472ba
RobbinBouwmeester <robbin.bouwmeester@ugent.be>
2025-03-11 09:34:25 +0100

    Update constants.py

--- proteobench/modules/constants.py
+++ proteobench/modules/constants.py
@@ -8,8 +8,8 @@ IO_PARSE_SETTINGS_DIR = pathlib.Path(__file__) / ".." / ".." / "io" / "parsing" 
 QUANT_LFQ_SETTINGS_DIR = (IO_PARSE_SETTINGS_DIR / "Quant" / "lfq").resolve()
 
 MODULE_SETTINGS_DIRS = {
-    "quant_lfq_ion_DIA_AIF": (QUANT_LFQ_SETTINGS_DIR / "ion" / "DIA" / "AIF").as_posix(),
-    "quant_lfq_ion_DIA_diaPASEF": (QUANT_LFQ_SETTINGS_DIR / "ion" / "DIA" / "diaPASEF").as_posix(),
-    "quant_lfq_ion_DDA": (QUANT_LFQ_SETTINGS_DIR / "ion" / "DDA").as_posix(),
-    "quant_lfq_peptidoform_DDA": (QUANT_LFQ_SETTINGS_DIR / "peptidoform" / "DDA").as_posix(),
+    "quant_lfq_ion_DIA_AIF": (QUANT_LFQ_SETTINGS_DIR / "DIA" / "ion" / "AIF").as_posix(),
+    "quant_lfq_ion_DIA_diaPASEF": (QUANT_LFQ_SETTINGS_DIR / "DIA" / "ion" / "diaPASEF").as_posix(),
+    "quant_lfq_ion_DDA": (QUANT_LFQ_SETTINGS_DIR / "DDA" / "ion").as_posix(),
+    "quant_lfq_peptidoform_DDA": (QUANT_LFQ_SETTINGS_DIR / "DDA" / "peptidoform").as_posix(),
 }

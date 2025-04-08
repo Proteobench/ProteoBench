79dd378c2e880ae6a6407c0a2a2f1ae411111808
veitveit <veits@bmb.sdu.dk>
2025-04-08 21:41:35 +0200

    fixing test

--- test/test_parse_params_wombat.py
+++ test/test_parse_params_wombat.py
@@ -24,6 +24,4 @@ def test_read_wombat_settings(file):
     actual = pd.Series(actual.__dict__)
     actual = pd.read_csv(io.StringIO(actual.to_csv()), index_col=0).squeeze("columns")
     expected = expected.loc[actual.index]
-    print(actual)
-    print(expected)
     assert expected.equals(actual)

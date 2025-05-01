9d05e88294f36a4cd377605df656ed185e7339ab
rodvrees <robbe.devreese@hotmail.com>
2025-05-01 21:12:38 +0200

    Format tolerances and black

--- proteobench/io/params/alphadia.py
+++ proteobench/io/params/alphadia.py
@@ -13,7 +13,7 @@ from proteobench.io.params import ProteoBenchParameters
 # Regular expression to clean up lines from ANSI escape codes
 ANSI_REGEX = re.compile(r"(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]")
 TIMESTAMP_REGEX = re.compile(r"(\d+ day,)|(\d+):\d{2}:\d{2}\.?\d*")
-DEBUG_LEVEL_REGEX = re.compile(r'(PROGRESS|INFO|WARNING|ERROR|CRITICAL|DEBUG):')
+DEBUG_LEVEL_REGEX = re.compile(r"(PROGRESS|INFO|WARNING|ERROR|CRITICAL|DEBUG):")
 TREE_REGEX = re.compile(r"^\s*(├──|└──|\│)\s*|\s*(├──|└──|\│)\s*")
 USER_DEFINED_REGEX = re.compile(r"(\[|\()?user defined(\]|\))?")
 DEFAULT_REGEX = re.compile(r"(\[|\()?default:?(\]|\))?")
@@ -23,7 +23,7 @@ CONFIG_KEY_MAPPER = {
     "software_name": "software_name",
     "search_engine": "search_engine",
     "search_engine_version": "search_engine_version",
-    "fdr":["ident_fdr_psm", "ident_fdr_protein"],
+    "fdr": ["ident_fdr_psm", "ident_fdr_protein"],
     "mbr_step_enabled": "enable_match_between_runs",
     "target_ms1_tolerance": "precursor_mass_tolerance",
     "target_ms2_tolerance": "fragment_mass_tolerance",
@@ -41,6 +41,7 @@ CONFIG_KEY_MAPPER = {
     "predictors_library": "predictors_library",
 }
 
+
 def clean_line(line: str) -> str:
     """Clean up a line by removing ANSI escape codes and trimming whitespace, as well as removing timestamps."""
     line = ANSI_REGEX.sub("", line)
@@ -49,10 +50,11 @@ def clean_line(line: str) -> str:
     line = TREE_REGEX.sub("", line)
     return line.strip()
 
+
 def parse_key_value(line: str) -> Tuple[str, str]:
     """
     Parse a key-value pair from a line in the log. It assumes the format 'key: value'.
-    
+
     Parameters
     ----------
     line : str
@@ -65,6 +67,7 @@ def parse_key_value(line: str) -> Tuple[str, str]:
     """
     key, value = line.split(":", 1)
     return key.strip(), value.strip()
+
 
 def extract_values_from_nested_lines(lines: List[str], start_index: int) -> List[int]:
     """
@@ -88,12 +91,12 @@ def extract_values_from_nested_lines(lines: List[str], start_index: int) -> List
         if len(values) == 3:
             break
         cleaned_line = clean_line(line)
-        
+
         if cleaned_line:
             if "user defined" in cleaned_line:
                 cleaned_line = USER_DEFINED_REGEX.sub("", cleaned_line)
                 number = re.search(r"\d+", cleaned_line)
-                
+
                 if number and values and not newer_version:
                     values[-1] = number.group(0)
                 elif number:
@@ -108,17 +111,18 @@ def extract_values_from_nested_lines(lines: List[str], start_index: int) -> List
 
     return values[:-1]
 
+
 def read_file_lines(file_path: str) -> List[str]:
     """Read lines from a file."""
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         return f.readlines()
 
 
 def initialize_default_parameters() -> Dict[str, str]:
     """Initialize default parameters."""
     return {
-        'software_name': "AlphaDIA",
-        'search_engine': "AlphaDIA",
+        "software_name": "AlphaDIA",
+        "search_engine": "AlphaDIA",
         "quantification_method": "DirectLFQ",
         "predictors_library": "AlphaPeptDeep",
     }
@@ -171,7 +175,7 @@ def clean_up_parameters(all_parameters: Dict[str, str]) -> None:
     for key in keys_to_remove:
         del all_parameters[key]
 
-    for key, value in all_parameters.items(): 
+    for key, value in all_parameters.items():
         if isinstance(value, str):
             value_list = list(set(value.split(", ")))
             if len(value_list) == 1:
@@ -180,12 +184,13 @@ def clean_up_parameters(all_parameters: Dict[str, str]) -> None:
                 for val in value_list:
                     if "default" in val:
                         all_parameters[key] = DEFAULT_REGEX.sub("", val).replace("]", "").strip()
-                        
+
                     if "user defined" in val:
                         all_parameters[key] = USER_DEFINED_REGEX.sub("", val)
                         break
         elif isinstance(value, list):
             all_parameters[key] = list(set(value))
+
 
 def extract_params(file_path: str) -> Dict[str, str]:
     """
@@ -218,8 +223,16 @@ def extract_params(file_path: str) -> Dict[str, str]:
 
     map_keys_to_desired_format(all_parameters)
     clean_up_parameters(all_parameters)
+    # Format some values
+    all_parameters["precursor_mass_tolerance"] = (
+        "[-" + all_parameters["precursor_mass_tolerance"] + "ppm," + all_parameters["precursor_mass_tolerance"] + "ppm]"
+    )
+    all_parameters["fragment_mass_tolerance"] = (
+        "[-" + all_parameters["fragment_mass_tolerance"] + "ppm, " + all_parameters["fragment_mass_tolerance"] + "ppm]"
+    )
 
     return ProteoBenchParameters(**all_parameters)
+
 
 if __name__ == "__main__":
     for fname in [

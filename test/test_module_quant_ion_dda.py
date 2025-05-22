83b5e678ea3db43ad0200981d389793f4050bc08
cajac102 <ca.jachmann@gmail.com>
2025-05-22 13:59:43 +0200

    update tests to new name

--- test/test_module_quant_ion_dda.py
+++ test/test_module_quant_ion_dda.py
@@ -59,7 +59,7 @@ class TestSoftwareToolOutputParsing:
     @pytest.mark.parametrize("software_tool", TESTED_SOFTWARE_TOOLS)
     def test_valid_and_supported_search_tool_settings_exists(self, software_tool):
         parse_settings_builder = ParseSettingsBuilder(
-            parse_settings_dir=PARSE_SETTINGS_DIR, module_id="quant_lfq_DDA_ion"
+            parse_settings_dir=PARSE_SETTINGS_DIR, module_id="quant_lfq_DDA_ion_QExactive"
         )
         assert software_tool in parse_settings_builder.INPUT_FORMATS
 
@@ -71,7 +71,7 @@ class TestSoftwareToolOutputParsing:
     @pytest.mark.parametrize("software_tool", TESTED_SOFTWARE_TOOLS)
     def test_settings_parser_created_successfully(self, software_tool):
         parse_settings_builder = ParseSettingsBuilder(
-            parse_settings_dir=PARSE_SETTINGS_DIR, module_id="quant_lfq_DDA_ion"
+            parse_settings_dir=PARSE_SETTINGS_DIR, module_id="quant_lfq_DDA_ion_QExactive"
         )
         parse_settings = parse_settings_builder.build_parser(software_tool)
         assert parse_settings is not None
@@ -79,7 +79,7 @@ class TestSoftwareToolOutputParsing:
     @pytest.mark.parametrize("software_tool", TESTED_SOFTWARE_TOOLS)
     def test_software_tool_output_converted_to_standard_format(self, software_tool):
         parse_settings_builder = ParseSettingsBuilder(
-            parse_settings_dir=PARSE_SETTINGS_DIR, module_id="quant_lfq_DDA_ion"
+            parse_settings_dir=PARSE_SETTINGS_DIR, module_id="quant_lfq_DDA_ion_QExactive"
         )
         parse_settings = parse_settings_builder.build_parser(software_tool)
 
@@ -94,7 +94,7 @@ class TestQuantScores:
     @pytest.mark.parametrize("software_tool", TESTED_SOFTWARE_TOOLS)
     def test_intermediate_generated_from_software_tool_output(self, software_tool):
         parse_settings_builder = ParseSettingsBuilder(
-            parse_settings_dir=PARSE_SETTINGS_DIR, module_id="quant_lfq_DDA_ion"
+            parse_settings_dir=PARSE_SETTINGS_DIR, module_id="quant_lfq_DDA_ion_QExactive"
         )
         parse_settings = parse_settings_builder.build_parser(software_tool)
 

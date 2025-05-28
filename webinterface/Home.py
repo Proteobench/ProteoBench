ac87a56d21ad46748e9fcd02877a4e09d0dea20d
rodvrees <robbe.devreese@hotmail.com>
2025-05-28 10:02:50 +0200

    Add stat widgets

--- webinterface/Home.py
+++ webinterface/Home.py
@@ -1,5 +1,6 @@
 """Proteobench Streamlit-based web server."""
 
+import streamlit as st
 from _base import StreamlitPage
 
 
@@ -8,11 +9,72 @@ class StreamlitPageHome(StreamlitPage):
     This class sets up the main page layout for the Streamlit application.
     """
 
+    @staticmethod
+    def stat_box(title, value, icon, color):
+        return f"""
+            <div style="
+                background-color: #ffffff;
+                border-radius: 12px;
+                padding: 16px 20px;
+                box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05);
+                display: flex;
+                align-items: center;
+                height: 100px;
+            ">
+                <div style="
+                    font-size: 28px;
+                    margin-right: 16px;
+                    color: {color};
+                ">{icon}</div>
+                <div>
+                    <div style='font-size: 14px; color: #666;'>{title}</div>
+                    <div style='font-size: 24px; font-weight: 600; color: #222;'>{value}</div>
+                </div>
+            </div>
+            """
+
     def _main_page(self):
         """
         Set up the main page layout for the Streamlit application.
         """
-        pass
+        # Placeholders TODO: replace with actual data
+        n_modules_all = 12345
+        n_modules_reviewed = 6789
+        n_tools_supported = 100
+        n_of_points_submitted = 5000  # TODO
+        monthly_visitors = 100
+
+        st.header("ProteoBench Overview")
+
+        # First row
+        row1_col1, row1_col2 = st.columns(2)
+        with row1_col1:
+            st.markdown(
+                self.stat_box("Number of modules (total)", n_modules_all, "🧩", "#37475E"), unsafe_allow_html=True
+            )
+        with row1_col2:
+            st.markdown(
+                self.stat_box("Number of modules (expert validated)", n_modules_reviewed, "✅", "#37475E"),
+                unsafe_allow_html=True,
+            )
+        st.markdown("<br>", unsafe_allow_html=True)
+        # Second row
+        row2_col1, row2_col2 = st.columns(2)
+        with row2_col1:
+            st.markdown(
+                self.stat_box("Number of tools supported", n_tools_supported, "🔨", "#37475E"), unsafe_allow_html=True
+            )
+        with row2_col2:
+            st.markdown(
+                self.stat_box("Number of submitted points", n_of_points_submitted, "⭕", "#37475E"),
+                unsafe_allow_html=True,
+            )
+        st.markdown("<br>", unsafe_allow_html=True)
+
+        # Third row
+        row3 = st.columns(1)
+        with row3[0]:
+            st.markdown(self.stat_box("Monthly visitors", monthly_visitors, "🌐", "#37475E"), unsafe_allow_html=True)
 
 
 if __name__ == "__main__":

4befbcbb35dd891d2d046b88f32e8423df2a387a
Henry Webel <heweb@dtu.dk>
2025-12-03 08:09:28 +0100

    :art: add docstring

--- webinterface/pages/base_pages/banner.py
+++ webinterface/pages/base_pages/banner.py
@@ -4,6 +4,8 @@ import streamlit as st
 # We do not have an abstract data class for variables yet, but
 # it is the one initialized in the pages_variables/Quant folder
 def display_banner(variables):
+    """Display a banner with warnings based on the variables provided on each tab
+    of the Quant modules."""
     if variables.archived_warning:
         st.info(variables.texts.ShortMessages.warning_archived)
     else:

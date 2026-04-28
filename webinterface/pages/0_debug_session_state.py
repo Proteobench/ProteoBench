ece5469577dec81514f7ad6315e3b6ae990b5261
Henry Webel <heweb@dtu.dk>
2026-04-28 17:04:19 +0200

    :art: add sidebar to debug page

--- webinterface/pages/0_debug_session_state.py
+++ webinterface/pages/0_debug_session_state.py
@@ -1,10 +1,12 @@
 """Debug session state page for development purposes."""
 
-import pandas as pd
+import pages.texts.proteobench_builder as pbb
 import streamlit as st
 from pages.base_pages.utils.session_state import ui_key_inspector, ui_overview_table
 
 st.title("Session State Debug")
+
+pbb.proteobench_sidebar(current_page="/")
 
 MAX_LIST_REPR = 5  # max items shown inline for lists
 

262035e764fae3684bc7383bf950079e9af8cf27
cajac102 <ca.jachmann@gmail.com>
2026-08-05 11:36:33 +0200

    entrapment no longer alpha

--- webinterface/pages/pages_variables/Entrapment/Entrapment_DIA_ion_Astral_variables.py
+++ webinterface/pages/pages_variables/Entrapment/Entrapment_DIA_ion_Astral_variables.py
@@ -87,7 +87,7 @@ class VariablesDIAEntrapmentAstral:
     first_new_plot: bool = True
     default_val_slider: int = 3
     max_nr_observed: int = 6
-    alpha_warning: bool = True
+    alpha_warning: bool = False
     beta_warning: bool = False
     archived_warning: bool = False
     enable_pmultiqc: bool = False
